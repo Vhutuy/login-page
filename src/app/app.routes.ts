c0d158791e762dcdf689d1e45cc955c31bdf17ca
wxnshw <wender.ribeiro8@gmail.com>
2025-04-15 00:32:05 -0300

    AnimalSignup Criada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { SignupComponent } from './pages/signup/signup.component';
 import { UserComponent } from './pages/user/user.component';
 import { AuthGuard } from './services/auth-guard.service';
 import { HomeComponent } from './pages/home/home.component';
+import { Component } from '@angular/core';
+import { AnimalSignupComponent } from './pages/animal-signup/animal-signup.component';
 
 
 export const routes: Routes = [
@@ -24,5 +26,10 @@ export const routes: Routes = [
         path: "home",
         component: HomeComponent,
         canActivate: [AuthGuard]
+    },
+    {
+        path: "animal-signup",
+        component: AnimalSignupComponent,
+        canActivate:[AuthGuard]
     }
 ];
---
+++ src/app/pages/animal-signup/animal-signup.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-animal-signup',
+  standalone: true,
+  imports: [],
+  templateUrl: './animal-signup.component.html',
+  styleUrl: './animal-signup.component.scss'
+})
+export class AnimalSignupComponent {
+
+}
